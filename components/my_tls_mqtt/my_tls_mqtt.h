32509411bae0293481780242d7e86bf75577fe7e
hescher <Github@johecker.com>
2024-12-17 19:41:33 +0100

    Update my_tls_mqtt.h

--- components/my_tls_mqtt/my_tls_mqtt.h
+++ components/my_tls_mqtt/my_tls_mqtt.h
@@ -1,41 +1,40 @@
 #pragma once
 
-#include "esphome.h"
-#include <WiFiClientSecure.h>
+#include "esphome/core/component.h"
 #include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+#include <BearSSLHelpers.h>
+#include <time.h>
+#include <string>
 
 namespace my_tls_mqtt {
 
 class MyTLSMQTTClient : public esphome::Component {
  public:
-  void set_host(const std::string &host, int port);
-  void set_username(const std::string &username);
-  void set_password(const std::string &password);
-  void set_discovery(bool discovery);
-  void set_discovery_prefix(const std::string &prefix);
-  void set_birth_message(const std::string &topic, const std::string &payload);
-  void set_will_message(const std::string &topic, const std::string &payload);
-
   void setup() override;
   void loop() override;
 
+  // Setter-Methoden für Konfigurationsparameter
+  void set_broker_host(const std::string &host);
+  void set_broker_port(uint16_t port);
+  void set_username(const std::string &username);
+  void set_password(const std::string &password);
+
  private:
+  // Private Methoden
+  void connect_to_mqtt_();
+
+  // Member-Variablen
+  std::string broker_host;
+  uint16_t broker_port;
+  std::string username_;
+  std::string password_;
+
+  bool initialized_ = false;  // Initialisierungstatus
+  BearSSL::X509List *trust_anchors_;  // Root-CA Zertifikate
+
   WiFiClientSecure wifi_client;
   PubSubClient mqtt_client;
-
-  std::string broker_host;
-  int broker_port;
-  std::string username;
-  std::string password;
-  bool discovery = false;
-  std::string discovery_prefix;
-
-  std::string birth_topic;
-  std::string birth_payload;
-  std::string will_topic;
-  std::string will_payload;
-
-  void reconnect();
 };
 
 }  // namespace my_tls_mqtt
