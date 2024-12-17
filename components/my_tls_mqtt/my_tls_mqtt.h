c12ce9f8f5b35081143d95e57de286cc16ec12a7
hescher <Github@johecker.com>
2024-12-17 17:15:46 +0100

    Update my_tls_mqtt.h

--- components/my_tls_mqtt/my_tls_mqtt.h
+++ components/my_tls_mqtt/my_tls_mqtt.h
@@ -6,23 +6,9 @@ namespace my_tls_mqtt {
 
 class MyTLSMQTTClient : public esphome::Component {
  public:
-  void set_host(const std::string &host, int port) {
-    this->broker_host = host;
-    this->broker_port = port;
-  }
-
-  void setup() override {
-    wifi_client.setCACert(root_ca);  // Root-CA festlegen
-    mqtt_client.setClient(wifi_client);
-    mqtt_client.setServer(broker_host.c_str(), broker_port);
-  }
-
-  void loop() override {
-    if (!mqtt_client.connected()) {
-      reconnect();
-    }
-    mqtt_client.loop();
-  }
+  void set_host(const std::string &host, int port);
+  void setup() override;
+  void loop() override;
 
  private:
   WiFiClientSecure wifi_client;
@@ -30,20 +16,12 @@ class MyTLSMQTTClient : public esphome::Component {
   std::string broker_host;
   int broker_port;
 
-  const char *root_ca = 
+  const char *root_ca =
     "-----BEGIN CERTIFICATE-----\n"
-    "... Root-CA hier einfügen ...\n"
+    "... dein Root-CA-Zertifikat hier einfügen ...\n"
     "-----END CERTIFICATE-----\n";
 
-  void reconnect() {
-    while (!mqtt_client.connected()) {
-      if (mqtt_client.connect("ESP8266_TLS_Client")) {
-        ESP_LOGD("TLSMQTT", "Connected to MQTT Broker over TLS");
-      } else {
-        delay(5000);
-      }
-    }
-  }
+  void reconnect();
 };
 
 }  // namespace my_tls_mqtt
