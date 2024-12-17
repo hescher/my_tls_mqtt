77255bf04fbca03ea816f0e60af1a01fe02e32c3
hescher <Github@johecker.com>
2024-12-17 17:27:08 +0100

    Update my_tls_mqtt.cpp

--- components/my_tls_mqtt/my_tls_mqtt.cpp
+++ components/my_tls_mqtt/my_tls_mqtt.cpp
@@ -7,8 +7,16 @@ void MyTLSMQTTClient::set_host(const std::string &host, int port) {
   this->broker_port = port;
 }
 
+void MyTLSMQTTClient::set_username(const std::string &username) {
+  this->username = username;
+}
+
+void MyTLSMQTTClient::set_password(const std::string &password) {
+  this->password = password;
+}
+
 void MyTLSMQTTClient::setup() {
-  static BearSSL::X509List cert(root_ca);  // Root-CA als TrustAnchor laden
+  static BearSSL::X509List cert(root_ca);
   wifi_client.setTrustAnchors(&cert);
 
   mqtt_client.setClient(wifi_client);
@@ -24,7 +32,7 @@ void MyTLSMQTTClient::loop() {
 
 void MyTLSMQTTClient::reconnect() {
   while (!mqtt_client.connected()) {
-    if (mqtt_client.connect("ESP_TLS_Client")) {
+    if (mqtt_client.connect("ESP_TLS_Client", username.c_str(), password.c_str())) {
       esphome::ESP_LOGD("my_tls_mqtt", "Connected to MQTT Broker: %s", broker_host.c_str());
     } else {
       delay(5000);
