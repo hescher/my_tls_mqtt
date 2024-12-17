7aee18a664379db0fe9369e5734dde229a4bbd9f
hescher <Github@johecker.com>
2024-12-17 17:12:34 +0100

    Update __init__.py

--- components/my_tls_mqtt/__init__.py
+++ components/my_tls_mqtt/__init__.py
@@ -2,20 +2,17 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_HOST, CONF_PORT
 
-CODEOWNERS = ["@yourgithubusername"]  # Gib hier deinen GitHub-Namen an
-
 # Namespace erstellen
 my_tls_mqtt_ns = cg.esphome_ns.namespace("my_tls_mqtt")
 MyTLSMQTTClient = my_tls_mqtt_ns.class_("MyTLSMQTTClient", cg.Component)
 
-# Konfigurationsschema definieren
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MyTLSMQTTClient),
-    cv.Required(CONF_HOST): cv.string,  # Broker-Host
-    cv.Required(CONF_PORT): cv.port,    # Broker-Port
+    cv.Required(CONF_HOST): cv.string,
+    cv.Required(CONF_PORT): cv.port,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
     cg.add(var.set_host(config[CONF_HOST], config[CONF_PORT]))
+    await cg.register_component(var, config)
