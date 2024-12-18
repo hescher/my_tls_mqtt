544438971f55eeed1740a8ea53d7fc439b83b8be
hescher <Github@johecker.com>
2024-12-18 13:58:59 +0100

    Update __init__.py

--- components/tls_mqtt/__init__.py
+++ components/tls_mqtt/__init__.py
@@ -43,6 +43,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
+    cg.add_build_flag("-DLOG_TLS_MQTT")
     cg.add(var.set_broker_host(config[CONF_HOST]))
     cg.add(var.set_broker_port(config[CONF_PORT]))
 
